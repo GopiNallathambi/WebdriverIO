9009fb37826dbb312baa3a312149edddc6e437b3
GopiNallathambi <gopi.nallathambis@gmail.com>
2025-03-04 22:15:59 +0530

    Pause changed to 90000

--- WebdriverIO_Project_1/test/specs/test1.js
+++ WebdriverIO_Project_1/test/specs/test1.js
@@ -4,7 +4,7 @@ describe('Demo Test', function () {
     it('My First test', async () => {
         browser.url('https://google.com')
          await $('[type="search"]').setValue('WebDriverIO')
-        browser.pause(6000000)
+        browser.pause(9000000)
         await $('[name="btnm"]').click
         browser.pause(60000)
 
